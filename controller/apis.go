f5b57306c98fc5a8344537a0745b49c47ba7f6e1
xujianhua <xujianhuaap@gmail.com>
2016-05-27 18:09:59 +0800

    mysql

--- controller/apis.go
+++ controller/apis.go
@@ -1,11 +1,11 @@
 package controller
-
 const (
 	//欢迎界面
 	API_WELCOME="/welcome"
 
 	//用户系统
 	API_USER="/user"
+	API_USER_REGISTER="/user/register"
 	API_USER_FAVOURIE_PRODUC="/user/favourite"
 
 	//产品系统
@@ -20,6 +20,7 @@ var Apis=[]string{
 	API_WELCOME,
 	//用户系统
 	API_USER,
+	API_USER_REGISTER,
 	API_USER_FAVOURIE_PRODUC,
 	//产品系统
 	API_PRODUCT,
@@ -27,8 +28,9 @@ var Apis=[]string{
 	API_DEAL,
 }
 // http:// 192.168.23.51:9090/welcome
-// http://192.168.23.51:9090/user
+// http://192.168.23.60:9090/user/register
 // http:// 192.168.23.51:9090/user/favourite
 // http:// 192.168.23.51:9090/product
 // http:// 192.168.23.51:9090/deal
+
 
--- main/main.go
+++ main/main.go
@@ -1,15 +1,18 @@
 package main
 import (
-	"github.com/astaxie/beego"
 	"github.com/xujianhuaap/geek/controller"
 	"github.com/xujianhuaap/geek/models"
 	"github.com/xujianhuaap/geek/utils"
 	"strings"
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	_"github.com/go-sql-driver/mysql"
 )
+
+var ormer orm.Ormer
 type MainController struct {
 	beego.Controller
 }
-
 func (this *MainController) Get() {
 	ctx:=this.Ctx;
 	if(strings.EqualFold(ctx.Request.RequestURI,controller.API_USER)){
@@ -25,11 +28,45 @@ func (this *MainController) Get() {
 		ctx.WriteString(ctx.Request.RequestURI)
 	}
 }
+func (this *MainController)Post() {
+	ctx:=this.Ctx;
+	if(strings.Contains(ctx.Request.RequestURI,controller.API_USER_REGISTER)){
+		user:=models.User{"xujianhua",001,27,true}
+		_,err:=ormer.Insert(user)
+		var response models.Response
+		if(err!=nil){
+			utils.PanicError(err)//错误诊断
+			response=models.Response{Code:300,Message:"失败",Content:new(models.User)}
+		}else{
+			response=models.Response{Code:200,Message:"成功",Content:user}
+		}
 
+		userStr,err:=utils.ToJson(response)
+		if(err==nil){
+			ctx.WriteString(userStr)
+		}else{
+			ctx.WriteString("-----------")
+			utils.PanicError(err)//错误诊断
+		}
+	}else{
+		ctx.WriteString(ctx.Request.RequestURI)
+	}
+}
 func main() {
+	//数据库初始化
+	ormer=orm.NewOrm()//全局执行一次
+	ormer.Using("default")
 	//自动注册路由
 	for _,api:=range controller.Apis {
 		beego.Router(api, &MainController{})
 	}
 	beego.Run()
 }
+
+func init()  {
+	maxIdle:=30;//最大的闲置数目
+	maxConnect:=30;//最大的在线访问
+	//orm.RegisterDriver("mysql", orm.DRMySQL) mysql已经默认注册过了
+	orm.RegisterDataBase("default","mysql","root:123456@/orm_geek?charset=utf8",maxIdle,maxConnect)
+}
+
